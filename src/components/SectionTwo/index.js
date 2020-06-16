ae34599b03a393124a10eeecb5c9e0412ac7aa87
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-06-16 04:46:34 -0500

    added cart data to index for testing

--- src/components/SectionTwo/index.js
+++ src/components/SectionTwo/index.js
@@ -10,6 +10,16 @@ function SectionTwoTitle() {
             <div className="row d-flex justify-content-center">
                 <div class="card product">
                     <img class="card-img" src="../assets/img/product/jers-007-blue.jpeg" alt="Card image" />
+                    <button className="snipcart-add-item product-definition btn btn-dark"
+                        data-item-id="ers-c01-01"
+                        data-item-name="JERS Tipografía Verde"
+                        data-item-price="240.00"
+                        data-item-image="../assets/img/product/jers-007-blue.jpeg"
+                        data-item-custom2-options="S|M|L"
+                        data-item-custom2-name="Tamaños"
+                        data-item-url="https://jersmx.netlify.app/tienda">
+                        Añadir al Carrito $240.00
+                    </button>
                 </div>
                 <div class="card product">
                     <img class="card-img" src="../assets/img/product/jers-013-w-black.jpg" alt="Card image" />
